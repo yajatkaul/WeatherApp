3da0065e44194daa02819d62d825c6f2a675aebf
yajatkaul <mc.crafter1973@gmail.com>
2024-04-07 00:23:49 +0530

    renamed:    images/clear.png -> clear.png 	renamed:    images/clouds.png -> clouds.png 	renamed:    images/drizzle.png -> drizzle.png 	renamed:    images/humidity.png -> humidity.png 	modified:   index.html 	renamed:    images/mist.png -> mist.png 	renamed:    images/rain.png -> rain.png 	modified:   script.js 	renamed:    images/search.png -> search.png 	renamed:    images/snow.png -> snow.png 	renamed:    images/wind.png -> wind.png

--- script.js
+++ script.js
@@ -17,27 +17,27 @@ async function checkweather(city){
     document.querySelector(".percentage").innerHTML = data.main.humidity + "%";
     if(data.weather[0].main == "Clouds")
     {
-        image.src = "/images/clouds.png"
+        image.src = "/clouds.png"
     }
     else if(data.weather[0].main == "Snow")
     {
-        image.src = "/images/snow.png"
+        image.src = "/snow.png"
     }
     else if(data.weather[0].main == "Rain")
     {
-        image.src = "/images/rain.png"
+        image.src = "/rain.png"
     }
     else if(data.weather[0].main == "Clear")
     {
-        image.src = "/images/clear.png"
+        image.src = "/clear.png"
     }
     else if(data.weather[0].main == "Mist")
     {
-        image.src = "/images/mist.png"
+        image.src = "/mist.png"
     }
     else if(data.weather[0].main == "Drizzle")
     {
-        image.src = "/images/drizzle.png"
+        image.src = "/drizzle.png"
     }
 }
 
