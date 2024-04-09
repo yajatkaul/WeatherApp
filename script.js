5ffac5edafebf5ea32a403bab8500bc5c90e8102
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2024-04-09 21:50:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,43 +5,38 @@ const image = document.querySelector(".weather-icon");
 
 
 
-async function checkweather(city){
+async function checkweather(city) {
     const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
     const response = await fetch(apiurl);
-    var data = await response.json();
+    const data = await response.json();
 
     console.log(data);
     document.querySelector(".name").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.wind.deg + "°C";
-    document.querySelector(".speed").innerHTML = data.wind.speed + "km/h";
+    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+    document.querySelector(".speed").innerHTML = data.wind.speed + " km/h";
     document.querySelector(".percentage").innerHTML = data.main.humidity + "%";
-    if(data.weather[0].main == "Clouds")
-    {
+    if (data.weather[0].main == "Clouds") {
         image.src = "images/clouds.png"
     }
-    else if(data.weather[0].main == "Snow")
-    {
+    else if (data.weather[0].main == "Snow") {
         image.src = "images/snow.png"
     }
-    else if(data.weather[0].main == "Rain")
-    {
+    else if (data.weather[0].main == "Rain") {
         image.src = "images/rain.png"
     }
-    else if(data.weather[0].main == "Clear")
-    {
+    else if (data.weather[0].main == "Clear") {
         image.src = "images/clear.png"
     }
-    else if(data.weather[0].main == "Mist")
-    {
+    else if (data.weather[0].main == "Mist") {
         image.src = "images/mist.png"
     }
-    else if(data.weather[0].main == "Drizzle")
-    {
+    else if (data.weather[0].main == "Drizzle") {
         image.src = "images/drizzle.png"
     }
 }
 
-searchbtn.addEventListener("click", ()=>{
+searchbtn.addEventListener("click", () => {
+    if(!cityname.value) return;
     checkweather(cityname.value);
     cityname.value = "";
 });
